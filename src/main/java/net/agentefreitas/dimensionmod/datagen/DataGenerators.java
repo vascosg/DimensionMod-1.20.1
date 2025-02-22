4f8812e1b5a90644d9e3a6fd487d6be440b77e7b
VascoS <vascosg@proton.me>
2025-02-22 20:21:26 +0000

    Added Datagen

--- src/main/java/net/agentefreitas/dimensionmod/datagen/DataGenerators.java
+++ src/main/java/net/agentefreitas/dimensionmod/datagen/DataGenerators.java
@@ -21,7 +21,7 @@ public class DataGenerators {
         CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
 
         generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput));
-        generator.addProvider(event.includeServer(), ModLootTableProvider.create(packOutput));
+        generator.addProvider(event.includeServer(), ModLootTableProvidor.create(packOutput));
 
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
---
+++ src/main/java/net/agentefreitas/dimensionmod/datagen/ModLootTableProvidor.java
@@ -0,0 +1,17 @@
+package net.agentefreitas.dimensionmod.datagen;
+
+import net.agentefreitas.dimensionmod.datagen.loot.ModBlockLootTables;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+
+import java.util.List;
+import java.util.Set;
+
+public class ModLootTableProvidor {
+    public static LootTableProvider create(PackOutput output) {
+        return new LootTableProvider(output, Set.of(), List.of(
+                new LootTableProvider.SubProviderEntry(ModBlockLootTables::new, LootContextParamSets.BLOCK)
+        ));
+    }
+}
